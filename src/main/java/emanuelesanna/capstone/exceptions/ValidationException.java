9b066e7a8840b3239d816e16ac4e0c931eeff787
EmanSan9 <sanna950@gmail.com>
2025-11-22 17:41:02 +0100

    Small fix

--- src/main/java/emanuelesanna/capstone/exceptions/ValidationException.java
+++ src/main/java/emanuelesanna/capstone/exceptions/ValidationException.java
@@ -9,7 +9,6 @@ public class ValidationException extends RuntimeException {
     private List<String> errorsMessages;
 
     public ValidationException(List<String> errorsMessages) {
-        super("Ci sono i seguenti errori di validazione: ");
         this.errorsMessages = errorsMessages;
     }
 }
