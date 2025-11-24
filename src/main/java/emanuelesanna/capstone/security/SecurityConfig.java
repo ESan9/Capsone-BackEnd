08767040c1d4025a4c144e947e20574aef41f45a
EmanSan9 <sanna950@gmail.com>
2025-11-24 16:47:38 +0100

    Update config

--- src/main/java/emanuelesanna/capstone/security/SecurityConfig.java
+++ src/main/java/emanuelesanna/capstone/security/SecurityConfig.java
@@ -63,7 +63,7 @@ public class SecurityConfig {
         CorsConfiguration configuration = new CorsConfiguration();
         configuration.setAllowedOriginPatterns(List.of(
                 "http://localhost:5173",
-                "https://capstone-frontend-git-main-emanueles-projects-95881b71.vercel.app"
+                "capstone-frontend-5vv83w690-emanueles-projects-95881b71.vercel.app"
         ));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
